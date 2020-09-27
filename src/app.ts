b1f9e39535a66f9fa407d2a81e54f5b7a0f15502
juan-gomez <juan.gomez@workday.com>
2020-09-27 08:52:35 +0100

    Reading two channels camera and acceleremeter read

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-const noble = require('@abandonware/noble');
-const chalk = require('chalk');
-// https://www.playemulator.com/nes-online/super-mario-bros/
-var robot = require("robotjs");
+import  noble, { Peripheral, state } from '@abandonware/noble';
+import chalk from 'chalk';
+//var robot = require("robotjs");
 
 const knownDevices = [];
 const LEGO_MARIO_MANUFACTURE_DATA = Buffer.from([0x97,0x03,0x00,0x43,0x0a,0x00,0x41,0x00]).toString('base64');
 const LWP_SERVICE_UUID = '00001623-1212-EFDE-1623-785FEABCD123';
 const LWP_CHARACTERISTIC_UUID = '00001624-1212-EFDE-1623-785FEABCD123';
+const SUBSCRIBE_IMU_ID = Buffer.from([0x0a,0x00,0x41,0x00,0x00,0x05,0x00,0x00,0x00,0x01]);
+const SUBSCRIBE_RGB_ID = Buffer.from([0x0a,0x00,0x41,0x01,0x01,0x05,0x00,0x00,0x00,0x01]);
 
 const PortId = {
   COLOR_BARCODE: 0x01,
@@ -14,37 +15,62 @@ const PortId = {
 };
 
 //discovered BLE device
-const discovered =  async (peripheral : any) => {
+const discovered =  async (peripheral : Peripheral) => {
     if(peripheral.advertisement.manufacturerData &&
-      peripheral.advertisement.manufacturerData.toString('base64') == LEGO_MARIO_MANUFACTURE_DATA)
-    {
+      peripheral.advertisement.manufacturerData.toString('base64') == LEGO_MARIO_MANUFACTURE_DATA)  {
       console.log('success to connect to LEGO Mario!\n')
       await peripheral.connectAsync();
+      const test = await peripheral.discoverAllServicesAndCharacteristicsAsync();
       const services = await peripheral.discoverServicesAsync([LWP_SERVICE_UUID]);
-      
       const characteristics = await services[0].discoverCharacteristicsAsync([LWP_CHARACTERISTIC_UUID]);
       const characteristic = characteristics[0];
       await characteristic.subscribeAsync();
       characteristic.on('data',(data : any, isNotification : any) => {
-        if(data.slice(0,4).toString('base64') == Buffer.from([0x07,0x00,0x45,0x01]).toString('base64')){
-          /*
-          const red = data[4];
-          const green = data[5];
-          const blue = data[6];
-          //console.log(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`);
-          console.log(chalk.hex(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`).bold('Color'));
-          //robot.setKeyboardDelay(5000);
-          //robot.keyToggle('right','down');
-          */
-         console.log(data.slice(1,3).toString('base64'));
-        } else {
-          console.log(data.slice(0,4).toString('base64'));
-        }
+        /*
+        switch (data[0]) {
+          case 7:
+            const x = data[4];
+            const y = data[5];
+            const z = data[6];
+            console.log(`${x.toString(16)} ${y.toString(16)}  ${z.toString(16)}`);
+            break;
+
+          case 8:
+            const red = data[4];
+            const green = data[5];
+            const blue = data[6];
+            console.log(chalk.hex(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`).bold('Color'));
+            break;
         
+          default:
+            break;
+        }
+        */
+        //robot.setKeyboardDelay(5000);
+        //robot.keyToggle('right','down');
+        console.log(data[0]+data[1]+data[2]);
+        /*
+        const red = data[4];
+            const green = data[5];
+            const blue = data[6];
+            console.log(chalk.hex(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`).bold('Color'));
+            */
       });
-      characteristic.write(Buffer.from([0x0a,0x00,0x41,0x01,0x01,0x05,0x00,0x00,0x00,0x01]),true);
+      
+      // Query the different channels 
+      while (peripheral.state == 'connected') {
+
+        // Subscribe to Accelerometer channel 
+        characteristic.write(SUBSCRIBE_IMU_ID, true);
+        // Subscribe to Camera channel 
+        await new Promise(resolve => setTimeout(resolve, 100));
+        characteristic.write(SUBSCRIBE_RGB_ID,true);
+      }
+      
     }
 }
+
+
 
 //BLE scan start
 const scanStart = () => {
