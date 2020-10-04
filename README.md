cd02152d601470fa9ca6e1275fc748d1dee3a30d
juan-gomez <juan.gomez@workday.com>
2020-10-04 13:00:32 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 # LEGO Mario 
 
-This is a code for using LEGO Mario as a controller
+This is a code for using LEGO Mario as a controller for playing on any generic emulator website like [Super Mario HTML5](https://supermarioemulator.com/supermario.php)
 
+If you need to change the keymap for your own emulator/game change the lines from here:
+
+That's it! enjoy!
+
+## Requisites
+
+- Node
+- Typescript
+- XCode: abandonaware/noble is the library used for Bluetooth communication needs xcode for building the module
+- A Lego Mario!
 
 ## Usage
 
+First be sure that Typescript is installed and then:
+
 ```
 npm install
-node src/app.js
+node js/app.js
 ```
 
 and push pairing button on back of LEGO Mario.
 
+# Disclaimer
+
+Only tested on MacOS 
+
+
 # Credits
 
 This project is inspired by the initial work of [nearprosmith](https://github.com/nearprosmith/legomario-on-mac)
+and the documentation from [Lego Mario Reverse Engineering](https://github.com/bricklife/LEGO-Mario-Reveng)
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import  noble, { Peripheral } from '@abandonware/noble';
 import chalk from 'chalk';
 import robot from 'robotjs';
 
+// Change for your own keyboard mapping here
+let keyLeft = 'left';
+let keyRight = 'right';
+let keyJump = 'up';
+
 
 const LEGO_MARIO_MANUFACTURE_DATA = Buffer.from([0x97,0x03,0x00,0x43,0x0a,0x00,0x41,0x00]).toString('base64');
 const LWP_SERVICE_UUID = '00001623-1212-EFDE-1623-785FEABCD123';
@@ -12,78 +17,93 @@ const SUBSCRIBE_RGB_ID = Buffer.from([0x0a,0x00,0x41,0x01,0x01,0x05,0x00,0x00,0x
 let posX = 0;
 let posY = 0;
 let posZ = 0;
+let jumpToogle = true;
+let pipeToogle = true;
 
 
 //discovered BLE device
 const discovered =  async (peripheral : Peripheral) => {
     if(peripheral.advertisement.manufacturerData &&
       peripheral.advertisement.manufacturerData.toString('base64') == LEGO_MARIO_MANUFACTURE_DATA)  {
-      console.log('success to connect to LEGO Mario!\n')
+      console.log('Success to connect to LEGO Mario!\n')
       await peripheral.connectAsync();
       const services = await peripheral.discoverServicesAsync([LWP_SERVICE_UUID]);
       const characteristics = await services[0].discoverCharacteristicsAsync([LWP_CHARACTERISTIC_UUID]);
       const characteristic = characteristics[0];
       await characteristic.subscribeAsync();
       characteristic.on('data',(data : any, isNotification : any) => {
+        console.log(data[0]);
         switch (data[0]) {
           // Position
-          case 7:
+          case 10:
             const x = data[4];
             const y = data[5];
             const z = data[6];
-            //console.log(`X ${x } Y ${y}  Z ${z}`);
-            //robot.setKeyboardDelay(5000);
-            //robot.keyToggle('right','down');
-            movement(x, y , z);
+            console.log(`Accelerometer X: ${x} Y:${y} Z:${z}`);
+            //movement(x, y , z);
             
             break;
           // Camera
-          case 8:
+          case 7:
             const red = data[4];
             const green = data[5];
             const blue = data[6];
-            console.log(chalk.hex(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`).bold('Color'));
+            //console.log(`color X: ${red} Y:${green} Z:${blue}`);
+            //console.log(chalk.hex(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`).bold('Color'));
+            //movementPipes(red, green, blue);
             break;
         
           default:
             break;
         }
-        
-        
-        //console.log(data[0]+data[1]+data[2]);
-        /*
-        const red = data[4];
-            const green = data[5];
-            const blue = data[6];
-            console.log(chalk.hex(`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`).bold('Color'));
-            */
+
       });
-      characteristic.write(SUBSCRIBE_IMU_ID, true);
-      /*
+
+      //characteristic.write(SUBSCRIBE_RGB_ID,true);
+      //characteristic.write(SUBSCRIBE_IMU_ID, true);
+      
       // Query the different channels 
-      while (peripheral.state == 'connected') {
+      console.log('saaaaaaaaaa' + peripheral.state == 'connected');
+      while (true) {
 
         // Subscribe to Accelerometer channel 
         characteristic.write(SUBSCRIBE_IMU_ID, true);
+        
+        await new Promise(resolve => setTimeout(resolve, 120));
+
         // Subscribe to Camera channel 
-        await new Promise(resolve => setTimeout(resolve, 100));
         characteristic.write(SUBSCRIBE_RGB_ID,true);
       }
-      */
+      
       
     }
 }
 
+async function movementPipes(red : Number , green : Number , blue : Number) {
+  if(blue.toString(16) == "0x25") {
+    console.log('Greeen')
+    robot.keyToggle('down','down');
+    pipeToogle = true;
+  } else {
+    if(pipeToogle) {
+      robot.keyToggle('down','up');
+      pipeToogle = false;
+    }
+  }
+}
+
+
+// The move
 async function movement ( x: Number , y : number , z : number)  {
   let testX = true;
   let posY = false;
   let testZ = true;
-  let jumpToogle = true;
 
+  // Jump
   if(testX) {
     console.log(x);
     if(x < 100 ) {
-      robot.keyToggle('up','down');
+      robot.keyToggle(keyJump,'down');
       jumpToogle = true;
       console.log('x');
     } else {
@@ -99,12 +119,12 @@ async function movement ( x: Number , y : number , z : number)  {
     //console.log('z : ' + z  + 'posZ : '+ posZ);
     if(z < 100 && z < posZ) {
       console.log('press left');
-      robot.keyToggle('right','up');
-      robot.keyToggle('left','down');
+      robot.keyToggle(keyRight,'up');
+      robot.keyToggle(keyLeft,'down');
     } else if(z > 100 && z > posZ) {
       console.log('press right');
-      robot.keyToggle('left','up');
-      robot.keyToggle('right','down');
+      robot.keyToggle(keyLeft,'up');
+      robot.keyToggle(keyRight,'down');
     } 
     posZ = z;
   }
@@ -114,7 +134,7 @@ async function movement ( x: Number , y : number , z : number)  {
 
 //BLE scan start
 const scanStart = () => {
-  console.log('finding LEGO Mario...')
+  console.log('Finding LEGO Mario...')
     noble.startScanning();
     noble.on('discover', discovered);
 }
